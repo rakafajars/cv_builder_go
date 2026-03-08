585c005a7e2e9a5ea45cce87a71209efc5861d58
Raka Fajar Salinggih <73903697+rakafajars@users.noreply.github.com>
2026-03-08 23:47:21 +0700

    feat: add date validation logic in work experience usecase

--- internal/usecase/work_experience_use_case.go
+++ internal/usecase/work_experience_use_case.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"cv-builder-api/internal/models"
 	"cv-builder-api/internal/repository"
+	"errors"
 )
 
 type WorkExperienceUsecase interface {
@@ -26,12 +27,42 @@ func (u *workExperienceUsecase) GetAllByUserID(userID uint) ([]models.WorkExperi
 	return u.repo.GetAllByUserID(userID)
 }
 
-func (u *workExperienceUsecase) Create(workExperience *models.WorkExperience) error {
-	return u.repo.Create(workExperience)
+func (u *workExperienceUsecase) Create(experience *models.WorkExperience) error {
+	// pengecekan isCurrent jika masih bekerja
+	// 1. flow jika is_current = true (Masih Kerja)
+	if experience.IsCurrent {
+		experience.EndDate = nil
+	} else {
+		// ATURAN 2: Kalau is_current = false (Sudah resign)
+		// Pastikan end_date tidak kosong
+		if experience.EndDate == nil {
+			return errors.New("Tanggal selesai wabjib diisi jika sudah tidak bekerja")
+		}
+
+		if experience.EndDate.Before(experience.StartDate) {
+			return errors.New("tanggal selesai tidak boleh sebelum tanggal mulai")
+		}
+	}
+	return u.repo.Create(experience)
 }
 
-func (u *workExperienceUsecase) Update(userID, ID uint, workExperience *models.WorkExperience) error {
-	return u.repo.Update(userID, ID, workExperience)
+func (u *workExperienceUsecase) Update(userID, ID uint, experience *models.WorkExperience) error {
+	// pengecekan isCurrent jika masih bekerja
+	// 1. flow jika is_current = true (Masih Kerja)
+	if experience.IsCurrent {
+		experience.EndDate = nil
+	} else {
+		// ATURAN 2: Kalau is_current = false (Sudah resign)
+		// Pastikan end_date tidak kosong
+		if experience.EndDate == nil {
+			return errors.New("Tanggal selesai wabjib diisi jika sudah tidak bekerja")
+		}
+
+		if experience.EndDate.Before(experience.StartDate) {
+			return errors.New("tanggal selesai tidak boleh sebelum tanggal mulai")
+		}
+	}
+	return u.repo.Update(userID, ID, experience)
 }
 
 func (u *workExperienceUsecase) Delete(userID, id uint) error {
